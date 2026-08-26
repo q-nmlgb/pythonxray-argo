03c3390d1f01b7d1775a75f7262bce4c3f5236e8
Tad Stiedemann <71119932+q-nmlgb@users.noreply.github.com>
2026-08-26 15:30:40 +0800

    Update http_server.py

--- http_server.py
+++ http_server.py
@@ -2,7 +2,7 @@ import modal
 
 app = modal.App("simple")
 
-image = modal.Image.from_dockerfile("Dockerfile").add_local_file("D:/work/123/app.py", remote_path="/root/app.py")
+image = modal.Image.from_dockerfile("Dockerfile").add_local_file("./app.py", remote_path="/root/app.py")
 
 @app.function(
     image=image,
@@ -12,7 +12,7 @@ image = modal.Image.from_dockerfile("Dockerfile").add_local_file("D:/work/123/ap
     scaledown_window=300,   # 闲置 300 秒无请求自动休眠
     timeout=86400,
 )
-@modal.web_server(port=8001, startup_timeout=60)
+@modal.web_server(port=3000, startup_timeout=60)
 def run_server():
     import subprocess
     # 使用 Popen 后台非阻塞启动 Python 简易 HTTP 服务器
